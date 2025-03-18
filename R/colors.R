99dc798b1d6f0a6d0335e82e6c5f1b189d140296
James <JesusButForGayPeople@proton.me>
2025-03-17 22:05:58 -0400

    first commit

---
+++ R/colors.R
@@ -0,0 +1,139 @@
+#' Deep Red color
+#' @export
+Deep.Red <- "#590D21"
+
+#' Dark Red color
+#' @export
+Dark.Red <- "#9F193D"
+
+#' Red color
+#' @export
+Red <- "#C31E4A"
+
+#' Light Red color
+#' @export
+Light.Red <- "#E66084"
+
+#' Pale Red color
+#' @export
+Pale.Red <- "#F1A7BB"
+
+#' Deep Orange color
+#' @export
+Deep.Orange <- "#6F4B0B"
+
+#' Dark Orange color
+#' @export
+Dark.Orange <- "#A77011"
+
+#' Orange color
+#' @export
+Orange <- "#E99F1F"
+
+#' Light Orange color
+#' @export
+Light.Orange <- "#F0BF6A"
+
+#' Pale Orange color
+#' @export
+Pale.Orange <- "#F4CF90"
+
+#' Deep Yellow color
+#' @export
+Deep.Yellow <- "#9D7F06"
+
+#' Dark Yellow color
+#' @export
+Dark.Yellow <- "#CEA708"
+
+#' Yellow color
+#' @export
+Yellow <- "#E8D206"
+
+#' Light Yellow color
+#' @export
+Light.Yellow <- "#FFE373"
+
+#' Pale Yellow color
+#' @export
+Pale.Yellow <- "#FFF8DC"
+
+#' Deep Green color
+#' @export
+Deep.Green <- "#304011"
+
+#' Dark Green color
+#' @export
+Dark.Green <- "#54711E"
+
+#' Green color
+#' @export
+Green <- "#83B02F"
+
+#' Light Green color
+#' @export
+Light.Green <- "#ABD45E"
+
+#' Pale Green color
+#' @export
+Pale.Green <- "#C4E18E"
+
+#' Deep Blue color
+#' @export
+Deep.Blue <- "#002429"
+
+#' Dark Blue color
+#' @export
+Dark.Blue <- "#004852"
+
+#' Blue color
+#' @export
+Blue <- "#008C9E"
+
+#' Light Blue color
+#' @export
+Light.Blue <- "#1FE5FF"
+
+#' Pale Blue color
+#' @export
+Pale.Blue <- "#85F1FF"
+
+#' Deep Purple color
+#' @export
+Deep.Purple <- "#271041"
+
+#' Dark Purple color
+#' @export
+Dark.Purple <- "#4E2183"
+
+#' Purple color
+#' @export
+Purple <- "#743496"
+
+#' Light Purple color
+#' @export
+Light.Purple <- "#A06CDA"
+
+#' Pale Purple color
+#' @export
+Pale.Purple <- "#CAADEB"
+
+#' Deep Grey color
+#' @export
+Deep.Grey <- "#151315"
+
+#' Dark Grey color
+#' @export
+Dark.Grey <- "#403A3F"
+
+#' Grey color
+#' @export
+Grey <- "#6F646C"
+
+#' Light Grey color
+#' @export
+Light.Grey <- "#9E949B"
+
+#' Pale Grey color
+#' @export
+Pale.Grey <- "#CFC9CD"
